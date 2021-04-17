1dc73bced518f92a90a10d030597f361b0fe99d4
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-04-17 14:10:22 +0530

    init styling

---
+++ lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+const gradient_butt =
+    LinearGradient(colors: [Color(0xFF5468E6), Color(0xFF654BDB)]);
+const gradient2 = LinearGradient(colors: [Color(0xFF6FB369),Color(0xFFE4CD44)]);
+var font_def = GoogleFonts.poppins(color:Colors.white);
---
+++ lib/screens/startupPage.dart
@@ -0,0 +1,75 @@
+import 'dart:ui';
+
+import 'package:cathacks/constants.dart';
+import 'package:easy_gradient_text/easy_gradient_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:nice_button/nice_button.dart';
+
+class StartUpPage extends StatefulWidget {
+  @override
+  _StartUpPageState createState() => _StartUpPageState();
+}
+
+class _StartUpPageState extends State<StartUpPage> {
+  var firstColor = Color(0xff5b86e5), secondColor = Color(0xff36d1dc);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+          Image.asset('assets/background.webp', fit: BoxFit.cover),
+          ClipRRect(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 2, sigmaY: 2),
+              child: Container(
+                color: Colors.grey.withOpacity(0.1),
+                alignment: Alignment.center,
+                child: Center(
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        GestureDetector(
+                          onTap: () {print('oth');},
+                          child: Container(
+                              width: 260,
+                              height:60,
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(20),
+                                  gradient: gradient_butt),
+                              child: Padding(
+                                  padding: EdgeInsets.all(10),
+                                  child: Text(
+                                    "For Teachers",
+                                    style: font_def,
+                                  ))),
+                        ),
+                        SizedBox(height: 10),
+                        Container(
+                            width: 260,
+                            height:60,
+                            alignment: Alignment.center,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(20),
+                                gradient: gradient2),
+                            child: Padding(
+                                padding: EdgeInsets.all(10),
+                                child: Text(
+                                  "For Students",
+                                  style: font_def,
+                                ))),
+                        
+                      ]),
+                ),
+              ),
+            ),
+          ),
+          Align(alignment: Alignment.bottomCenter,child: GradientText(text:'Made with ❤️ at CatHacks',style:GoogleFonts.roboto(fontSize: 30.0,color:Colors.white)))
+        ],
+      ),
+    );
+  }
+}
